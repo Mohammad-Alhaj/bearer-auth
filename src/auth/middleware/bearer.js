028118032c35bb9d55e53cb55af4cb9e271bfab3
Mohammad Alhaj <mohammadbasil.alhaj@gmail.com>
2022-06-25 16:04:19 +0300

    handle error

--- src/auth/middleware/bearer.js
+++ src/auth/middleware/bearer.js
@@ -1,20 +1,29 @@
 'use strict';
 const User = require('../models/users-model')
 async function bearer(req,res,next){
-if(req.headers.authorization){
-const token = req.headers.authorization.split(" ")[1]
-User.authenticateBearer(token).then(()=>{
-    res.status(200).json({
-        'message': 'You are authorized to view the user orders',
-        'user': req.user
-    }); 
-    next();
-    
-})
-.catch(() => {
-    next("Invalid Token");
-})
 
+
+if(req.headers.authorization){
+    try{
+
+        const token = req.headers.authorization.split(" ")[1]
+        User.authenticateBearer(token).then(()=>{
+            res.status(200).json({
+                'message': 'You are authorized to view the user orders',
+                'user': req.user
+            }); 
+            next();
+            
+        })
+        .catch(() => {
+            next("Invalid Token");
+        })
+    }catch(err){
+        next(err)
+    }
+
+}else{
+    next('Token empty')
 }
 }
 module.exports =bearer;
--- src/auth/middleware/signup.js
+++ src/auth/middleware/signup.js
@@ -4,12 +4,16 @@ const User = require('../models/users-model')
 const bcrypt = require("bcrypt");
 
 const signup = (async(req,res,next)=>{
-    const username = req.body.username;
-    const password = await bcrypt.hash(req.body.password, 10);
-    const record = await User.create({ username: username, password: password });
-  res.status(201).json(record);
-
-next()
+    try{
+      const username = req.body.username;
+      const password = await bcrypt.hash(req.body.password, 10);
+      const record = await User.create({ username: username, password: password });
+    res.status(201).json(record);
+  
+  next()
+}catch(err){
+  next(err)
+}
 })
 
 module.exports = signup
